04dc86c26630fadcd53b67bbc55d352e3ab73f0b
Ismaeltg05 <156907266+Ismaeltg05@users.noreply.github.com>
2024-01-22 10:20:38 +0100

    ghaasfdf

--- app.java
+++ app.java
@@ -2,5 +2,6 @@ public class app {
     public static void main(String[] args) {
         System.out.println("HOLA");
         System.out.println("Adios");
+        System.out.println("Funciona??");
     }
 }
