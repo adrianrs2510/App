40646c288cf637b99d3b666ee92f53580a5a6a4f
adrianrs2510 <156907273+adrianrs2510@users.noreply.github.com>
2024-01-22 10:37:30 +0100

    a

--- app.java
+++ app.java
@@ -1,10 +1,5 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("HOLA");
-        System.out.println("Adios");
-        System.out.println("Funciona??");
-        System.out.println("isma");
-        System.out.println("AAAAAAAAAAAAAAAAAAA");
-        System.out.println("adri");
+        
     }
 }
